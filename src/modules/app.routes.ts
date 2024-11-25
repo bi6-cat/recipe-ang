ae5eeadd27115081fb9134762fd9257791c32b05
Nguyen Quang <quangnn906@gmail.com>
2024-11-25 20:16:43 +0700

    Step 3: Create manager module

---
+++ src/modules/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { ManagerLayoutComponent } from './shared/layouts/manager-layout/manager-layout.component';
+import { CustomerLayoutComponent } from './shared/layouts/customer-layout/customer-layout.component';
+
+export const routes: Routes = [
+    {
+        path: 'manager',
+        component: ManagerLayoutComponent,
+        loadChildren: () => import('./manager/manager.module').then(m => m.ManagerModule)
+    },
+    {
+        path: '',
+        component: CustomerLayoutComponent,
+        loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+    }
+];
